f2b012800d271503db395222b662f5eb5b24b7d8
TingSyuanWang <dtvgood202@gmail.com>
2016-11-06 14:54:50 +0800

    更新20161106

--- upload_file_advance.php
+++ upload_file_advance.php
@@ -1,7 +1,34 @@
 <?php
 	include("db_setting.php");
 		if (!@mysql_select_db("2018exporegister")) die("fail to connect to server!");
-		$sql_query = "INSERT INTO testadvance (`name`, `phone`, `telephone`, `identification`, `birthday`, `email`, `address`, `unit`, `position`, `food`, `event`, `attachment`) VALUES (";
+		// 寄信
+		$sendEmail = $_POST["email"];
+		$to = "$sendEmail"; //收件者
+		$subject = "【溫馨提醒】2018臺中世界花卉博覽會─導覽志工培訓報名成功資訊！"; //信件標題
+		$message = '<html><body>';
+		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">親愛的'.strip_tags($_POST['name']).'：';
+		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">您好。</p>';
+		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">感謝您報名「2018臺中世界花卉博覽會」導覽志工「<span style="color: red;">特殊教育訓練</span>」。<br/>';
+		$message .= '你報名的場次為：<span style="color: red;"><strong>'.strip_tags($_POST['event']).'</strong></span><br/>';
+		$message .= '提醒您，上課要攜帶「<span style="color: red;"><strong>志工基礎教育訓練結業證書影本</strong></span>」及「<span style="color: red;"><strong>2張1吋大頭照片</strong></span>」<br/>';
+		$message .= '詳細的上課地點及課程內容請參照報名系統之培訓場次，網址：<br/>';
+		$message .= '<a href="http://sls.asia.edu.tw/2018exporegister/index.html">http://sls.asia.edu.tw/2018exporegister/index.html</a></p>';
+		$message .= '<br/>';
+		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">「2018臺中世界花卉博覽會」導覽志工培訓，期待與您相會。<br/>';
+		$message .= '若有任何問題，歡迎來信或來電，我們將竭盡為您服務！</p><br/>';
+		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei; text-align: right;">亞洲大學學務處服務學習組敬上</p>';
+		$message .= '<p style="font-size:16px; font-family:Microsoft JhengHei;">服務專線：(04)23323456轉3295<br/>';
+		$message .= '服務時間：週一至週五08:10-17:00<br/>';
+		$message .= '服務信箱：<a href="mailto:yifong@asia.edu.tw">yifong@asia.edu.tw</a></p>';
+		$message .= '<img src="http://sls.asia.edu.tw/2018exporegister/images/logo.png" alt="2018臺中世界花卉博覽會" />';
+		$message .= '</body></html>';
+
+		// To send HTML mail, the Content-type header must be set
+		$headers = "Content-Type: text/html; charset=UTF-8". "\r\n";
+		$headers .= "From: 亞大花博伺服器-勿直接回信 <openstackicehousegeek@gmail.com>"; //寄件者
+		mail("$to", "$subject", "$message", "$headers");
+
+		$sql_query = "INSERT INTO applyadvance (`name`, `phone`, `telephone`, `identification`, `birthday`, `email`, `address`, `unit`, `position`, `food`, `event`, `attachment`) VALUES (";
 		$sql_query .=  "'".$_POST["name"]."',";
 		$sql_query .=  "'".$_POST["phone"]."',";
 		$sql_query .=  "'".$_POST["telephone"]."',";
@@ -34,32 +61,6 @@
 		// echo "Error";
 		//
 		// }
-		// 寄信
-		$sendEmail = $_POST["email"];
-		$to = "$sendEmail"; //收件者
-	  $subject = "【溫馨提醒】2018臺中世界花卉博覽會─導覽志工培訓報名成功資訊！"; //信件標題
-		$message = '<html><body>';
-		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">親愛的'.strip_tags($_POST['name']).'：';
-		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">您好。</p>';
-		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">感謝您報名「2018臺中世界花卉博覽會」導覽志工「<span style="color: red;">特殊教育訓練</span>」。<br/>';
-		$message .= '你報名的場次為：<span style="color: red;"><strong>'.strip_tags($_POST['event']).'</strong></span><br/>';
-		$message .= '提醒您，上課要攜帶「<span style="color: red;"><strong>志工基礎教育訓練結業證書影本</strong></span>」及「<span style="color: red;"><strong>2張1吋大頭照片</strong></span>」<br/>';
-		$message .= '詳細的上課地點及課程內容請參照報名系統之培訓場次，網址：<br/>';
-		$message .= '<a href="http://website/2018exporegister/index.html">http://website/2018exporegister/index.html</a></p>';
-		$message .= '<br/>';
-		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei;">「2018臺中世界花卉博覽會」導覽志工培訓，期待與您相會。<br/>';
-		$message .= '若有任何問題，歡迎來信或來電，我們將竭盡為您服務！</p><br/>';
-		$message .= '<p style="font-size:20px; font-family:Microsoft JhengHei; text-align: right;">亞洲大學學務處服務學習組敬上</p>';
-		$message .= '<p style="font-size:16px; font-family:Microsoft JhengHei;">服務專線：(04)23323456轉3295<br/>';
-		$message .= '服務時間：週一至週五08:10-17:00<br/>';
-		$message .= '服務信箱：<a href="mailto:yifong@asia.edu.tw">yifong@asia.edu.tw</a></p>';
-		$message .= '<img src="http://website/2018exporegister/images/logo.png" alt="2018臺中世界花卉博覽會" />';
-		$message .= '</body></html>';
-
-		// To send HTML mail, the Content-type header must be set
-		$headers = "Content-Type: text/html; charset=UTF-8". "\r\n";
-	  $headers .= "From: 亞大花博伺服器-勿直接回信 <openstackicehousegeek@gmail.com>"; //寄件者
-		mail("$to", "$subject", "$message", "$headers");
 ?>
 
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -100,7 +101,7 @@
 								</span>
 							</button>
 						</a>
-						<a href="http://website/2018expo">
+						<a href="http://sls.asia.edu.tw/2018expo">
 							<button type="button" class="btn btn-success btn-lg btn-block successButton">
 								<span>
 									回到首頁
